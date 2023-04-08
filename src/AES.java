c6b338a2fffdea3873321ddc9511c5bf383c5149
damino312 <daminionq@gmail.com>
2023-04-09 00:31:47 +0700

    connect AES, create dowloadHistory method

--- src/AES.java
+++ src/AES.java
@@ -8,11 +8,11 @@ public class AES {
 
     private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
     private static final String SECRET_KEY = "MySecretKey12345";
-    private static final String IV = "MyInitializationV";
+    private static final byte[] IV = new byte[16];;
 
     public static String encrypt(String plaintext) throws Exception {
         SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(IV.getBytes());
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
         byte[] encrypted = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
@@ -21,7 +21,7 @@ public class AES {
 
     public static String decrypt(String ciphertext) throws Exception {
         SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(IV.getBytes());
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
         byte[] decoded = Base64.getDecoder().decode(ciphertext);
--- src/ChatMessageDAO.java
+++ src/ChatMessageDAO.java
@@ -1,6 +1,9 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.time.LocalDateTime;
 
@@ -9,6 +12,7 @@ public class ChatMessageDAO {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "rootroot";
     private PrintStream os = null;
+    private static AES aes = new AES();
 
 
     public void saveMessage(String message, String sender, String recipient, LocalDateTime currentDate, Time currentTime) {
@@ -17,43 +21,87 @@ public class ChatMessageDAO {
             stmt.setString(1, sender);
             stmt.setString(2, recipient);
             stmt.setTime(3, currentTime);
-            stmt.setString(4, message);
+            String encryptedMessage = aes.encrypt(message);
+            byte[] encryptedMessageBytes = encryptedMessage.getBytes(StandardCharsets.UTF_8);
+
+            stmt.setBytes(4, encryptedMessageBytes);
             stmt.setTimestamp(5, Timestamp.valueOf(currentDate));
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void getHistory(Socket clientSocket, String clientName) throws SQLException {
         try (Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD)) {
-             Statement stmt = conn.createStatement();
-             String sql = "SELECT sender, recipient, time, message, date FROM chat";
-             ResultSet rs = stmt.executeQuery(sql);
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT sender, recipient, time, message, date FROM chat";
+            ResultSet rs = stmt.executeQuery(sql);
 
-             os = new PrintStream(clientSocket.getOutputStream());
+            os = new PrintStream(clientSocket.getOutputStream());
 
-             while(rs.next()) {
-                 String sender = rs.getString("sender");
-                 String recipient = rs.getString("recipient");
-                 String time = String.valueOf(rs.getTime("time"));
-                 String message = rs.getString("message");
-                 String date = rs.getString("date");
-                 if (recipient != null ) {
-                     if (recipient.equals(clientName)) {
-                         os.println(time +" "+ date + "Private message:"+ " >" + sender + "> " + message);
-
-                     } else {
-                         continue;
-                     }
-                 } else {
-                     os.println(time +" "+ date + " <" + sender + "> " + message);
-                 }
-             }
+            while (rs.next()) {
+                String sender = rs.getString("sender");
+                String recipient = rs.getString("recipient");
+                String time = String.valueOf(rs.getTime("time"));
+                String message = aes.decrypt(rs.getString("message"));
+                String date = rs.getString("date");
+                if (recipient != null) {
+                    if (recipient.equals(clientName)) {
+                        os.println(time + " " + date + " Private message:" + " >" + sender + "> " + message);
+                    } else if (sender.equals(clientName)) {
+                        os.println(time + " " + date + " Private message to " +recipient +": " + message);
+                    } else {
+                        continue;
+                    }
+                } else {
+                    os.println(time + " " + date + " <" + sender + "> " + message);
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+    }
+    public void downloadHistory(Socket clientSocket, String clientName) { // write into a file
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD)) {
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT sender, recipient, time, message, date FROM chat";
+            ResultSet rs = stmt.executeQuery(sql);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("history.txt"))) {
+                while (rs.next()) {
+                    String sender = rs.getString("sender");
+                    String recipient = rs.getString("recipient");
+                    String time = String.valueOf(rs.getTime("time"));
+                    String message = aes.decrypt(rs.getString("message"));
+                    String date = rs.getString("date");
+                    if (recipient != null) {
+                        if (recipient.equals(clientName)) {
+                            writer.write(time + " " + date + " Private message:" + " >" + sender + "> " + message);
+                            writer.newLine();
+                        } else if (sender.equals(clientName)) {
+                            writer.write(time + " " + date + " Private message to " +recipient +": " + message);
+                            writer.newLine();
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        writer.write(time + " " + date + " <" + sender + "> " + message);
+                        writer.newLine();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
